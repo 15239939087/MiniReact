59813c38724e2584a273f55b4761be8cea1c1182
chenwei <fengzhi.chen@tuya.com>
2022-02-16 11:03:43 +0800

    feat: JSX

--- src/MiniReact/react-dom.js
+++ src/MiniReact/react-dom.js
@@ -4,24 +4,30 @@
  * @param {container} 真实 DOM
  */
 export function render(element, container) {
+  const dom = createDom(element);
+  // 添加到容器
+  container.appendChild(dom);
+}
+
+/**
+ * 创建DOM
+ * @param {*} fiber fiber节点
+ * @returns dom 真实dom节点
+ */
+function createDom(fiber) {
   // 处理文本节点和标签节点
   const dom =
     element.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   // 为节点绑定属性
   const isProperty = (key) => key !== "children";
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-
-  // 处理孩子节点
-  element.props.children.forEach((child) => render(child, dom));
-
-  // 添加到容器
-  container.appendChild(dom);
+  return dom;
 }
