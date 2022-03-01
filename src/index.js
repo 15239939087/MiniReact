b18528d5318cf4f86f4e6acfa3f4deeca7412370
chenwei <fengzhi.chen@tuya.com>
2022-03-01 22:23:11 +0800

    feat: dev complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "./MiniReact";
 
-const element = <div id="foo">aaa</div>;
+const element = (
+  <div id="foo">
+    <div>我是测试少时诵诗书所</div>
+    <div>我是测试少时诵诗书所</div>
+    <div>我是测试少时诵诗书所</div>
+    <div>我是测试少时诵诗书所</div>
+    <div>我是测试少时诵诗书所</div>
+  </div>
+);
 
 React.render(element, document.getElementById("root"));
